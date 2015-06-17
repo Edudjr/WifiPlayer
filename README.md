72d5afc2de1a98295d65a3ea4947023d36e52bcb
Edudjr <eduardo.djr@hotmail.com>
2015-06-17 17:30:19 -0300

    updated package.json file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Project for a WifiPlayer using a Raspberry Pi
 
 ## How it works
-The project consist in Nodejs application (from this git) running in a Raspberry Pi, which is the server. The clients can access the service from their Laptops, cellphones or Tablets, through a browser. The application will search for specific shared Folders which countains the music. The clients can add their favorite music to the playlist.
+The project consist in a Node.js application (from this git) running in a Raspberry Pi, which is the server. The clients can access the service from their Laptops, cellphones or Tablets, through a browser. The application will search for specific shared Folders which countains the music. The clients can add their favorite music to the playlist.
 
 ## To do
 * Create list to pick the music from
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ var router = express.Router();
 var glob = require('glob');
 var fs = require('fs');
 var player = require('./player');
-
+var musicList = [];
+var musicPlaying = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var data = {};
 	data.title = 'WifiPlayer';
 	data.files = [];
+	data.playing = musicPlaying;
 
 	glob("/Users/Eduardo/Music/*.mp3", {mark: true}, function (er, files) {
 	  // files is an array of filenames. 
@@ -18,11 +20,11 @@ router.get('/', function(req, res, next) {
 	  // er is an error object or null.
 	  
 	  files.forEach(function(item){
-	  	player.addToPlaylist(item);
+	  	//player.addToPlaylist(item);
+	  	musicList.push(item);
 	  	data.files.push(player.getMetadata(item));
 	  });
 
-	  //data.files = files;
 	  //rende HTML and send info from files
 	  res.render('index', data);
 	});
@@ -50,6 +52,22 @@ router.get('/listmusic', function(req, res){
 	  // er is an error object or null.
 	  res.send(files);
 	});
+});
+
+router.get('/add', function(req, res){
+	var index = req.query.i;
+	//add music to playlist
+	player.addToPlaylist(musicList[index]);
+	//add music info to music playing (send to HTML)
+	musicPlaying.push(player.getMetadata(musicList[index]));
+	res.status(200).send();
+});
+
+router.get('/update', function(req, res){
+	new EJS
+	({
+	    url : 'http://' + host + '/'
+	}).update(list_playing, { data : musicPlaying });
 });
 
 router.get('/play',function(req, res){
